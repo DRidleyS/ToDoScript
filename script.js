233521bc4d8f4edaee8baf0b73245ebccbc28dca
Dorian <148677492+DRidleyS@users.noreply.github.com>
2023-12-14 13:46:54 -0800

    Update script.js
    correct commenting.

--- script.js
+++ script.js
@@ -1,69 +1,82 @@
+// Wait for the DOM to be fully loaded before executing the code
 document.addEventListener('DOMContentLoaded', function () {
+  // Select all list items within an unordered list
   var listItems = document.querySelectorAll('ul li');
 
+  // Loop through each list item
   listItems.forEach(function (item) {
-    // Attach click event listener to list item
+    // Attach a click event listener to each list item for strikethrough effect
     item.addEventListener('click', strikethrough);
 
-    // Add a delete button to each list item
+    // Create a delete button for each list item
     var deleteButton = document.createElement('button');
     deleteButton.classList.add('delete');
     deleteButton.textContent = 'Delete';
     item.appendChild(deleteButton);
 
-    // Attach click event listener to delete button
+    // Attach a click event listener to each delete button for removing the list item
     deleteButton.addEventListener('click', deleteListItem);
   });
 
+  // Function to get the length of the input value
   function inputLength() {
     return input.value.length;
   }
 
+  // Function to create a new list item
   function createListElement() {
+    // Create a new list item
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
 
-    // Add a delete button to the new list item
+    // Create a delete button for the new list item
     var deleteButton = document.createElement('button');
     deleteButton.classList.add('delete');
     deleteButton.textContent = 'Delete';
     li.appendChild(deleteButton);
 
+    // Append the new list item to the unordered list
     ul.appendChild(li);
-    input.value = "";
+    input.value = ""; // Clear the input field
 
-    // Attach click event listener to the new list item
+    // Attach a click event listener to the new list item for strikethrough effect
     li.addEventListener('click', strikethrough);
 
-    // Attach click event listener to delete button
+    // Attach a click event listener to the delete button for removing the new list item
     deleteButton.addEventListener('click', deleteListItem);
   }
 
+  // Function to add a new list item after a button click
   function addListAfterClick() {
     if (inputLength() > 0) {
       createListElement();
     }
   }
 
+  // Function to add a new list item after a keypress event (Enter key)
   function addListAfterKeypress(event) {
     if (inputLength() > 0 && event.keyCode === 13) {
       createListElement();
     }
   }
 
+  // Function for applying or removing strikethrough effect on a list item
   function strikethrough() {
     event.target.classList.toggle('done');
   }
 
+  // Function for deleting a list item
   function deleteListItem(event) {
     var listItem = event.target.parentElement;
     listItem.remove();
   }
 
+  // Get references to the button, input, and unordered list elements
   var button = document.getElementById("enter");
   var input = document.getElementById("userinput");
   var ul = document.querySelector("ul");
 
+  // Attach click event listeners to the button and input elements
   button.addEventListener("click", addListAfterClick);
   input.addEventListener("keypress", addListAfterKeypress);
 });
